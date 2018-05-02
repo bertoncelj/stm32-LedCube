fd53b8372f06098d335e06284426d8c0b804bad5
Tine Bertoncelj <king.tine@gmail.com>
2018-05-02 12:36:18 +0200

    Loading first data from exit. project

---
+++ inc/tlc5940.h
@@ -0,0 +1,118 @@
+#ifndef TLC5940_H
+#define TLC5940_H
+
+#include "stm32f4xx.h"
+#include "stm32f4xx_gpio.h"
+#include "defines.h"
+
+#define NUM_TLCS 3
+#define COUNT_TLC 3
+/*SPI1
+
+  *  GPIOA
+  *  MISO -> GPIO_Pin_6
+  *  MOSI -> GPIO_Pin_7
+  *  SCL  -> GPIO_Pin_5
+ */
+
+ #define TLC_RCC_AHB1 RCC_AHB1Periph_GPIOB
+ #define TLC5940_GPIO 	GPIOB
+ #define PIN_BLANK		GPIO_Pin_5
+ #define PIN_VPRG 		GPIO_Pin_9
+ #define PIN_XLAT 		GPIO_Pin_6
+ #define PIN_GSCLK		GPIO_Pin_3
+
+
+#define TLC5940_GPIO 	GPIOB
+#define PORTx_SPI 	 GPIOA
+#define DD_MOSI 	 GPIO_Pin_7		//SIN -> MOSI
+#define DD_SCK 		 GPIO_Pin_5		//Clock SCLK
+
+
+#define PORTx_TLC5940 	GPIOB
+#define DD_XLAT		 	GPIO_Pin_6
+#define DD_BLANK	 	GPIO_Pin_5
+#define DD_VPRG			GPIO_Pin_9		//MISO -> VPRG
+
+
+#define PORTx_GSCLK 	GPIOB
+#define DD_GSCLK 		GPIO_Pin_3	//ta je PWM ju definiran
+
+#define PORTx_PIN_LVL 	GPIOD
+#define PIN_LEVEL_1  	GPIO_Pin_7
+#define PIN_LEVEL_2	    GPIO_Pin_5
+#define PIN_LEVEL_3 	GPIO_Pin_3
+#define PIN_LEVEL_4 	GPIO_Pin_1
+/**
+ * Pin configuration
+ *
+ * 		OUT2		OUT1				tlc5940	tlc5940stm
+ * 		OUT3	D-> VPRG		PB4		VPRG	MISO
+ * 		OUT4		SIN			PD7		SS		MOSI
+ * 		OUT5		SCLK		PD5		SCK 	CLK
+ * 		OUT6	D->	XLAT		PD3		XLAT	XLAT
+ * 		OUT7	D->	BLANK		PD1		BLANK	BLANK
+ * 		OUT8	D->	GND
+ * 		OUT9	D->	VCC
+ * 		OUT10	D->	IREF
+ * 		OUT11	D->	DCPRG
+ * 		OUT12	D->	GSCLK		PA0		GSCL	PWM GPIO_Pin_0
+ * 		OUT13		SOUT
+ * 		OUT14		XERR
+ * 		OUT15		OUT16
+ *
+ *
+ *
+ *
+ *
+ * */
+
+
+/**
+ * @brief  Sets pin(s) high
+ * @note   Defined as macro to get maximum speed using register access
+ * @param  GPIOx: GPIOx PORT where you want to set pin high
+ * @param  GPIO_Pin: Select GPIO pin(s). You can select more pins with | (OR) operator to set them high
+ * @retval None
+ */
+#define GPIO_SetPinHigh(GPIOx, GPIO_Pin) 		((GPIOx)->BSRRL = (GPIO_Pin))
+
+/**
+ * @brief 	Set pin(s) low
+ * @note 	Define as a macro to get maximum speed using register acess
+ * @param	GPIOx: Port for which pin is set low
+ * @param	GPIO_Pin: Select GPIO pin. You can select more pins with | (OR) operator to set them low
+ * @retval	None
+ */
+#define GPIO_SetPinLow(GPIOx, GPIO_Pin) 		((GPIOx)->BSRRH = (GPIO_Pin))
+
+
+//array holding the data to be shifted in the driver
+uint16_t leds[15];
+
+//value for timer 2 GSCLK ?!? for now
+volatile uint16_t clkCnt;
+//timer for the PWM output
+void TIM2_IRQHandler(void);
+
+//Define functions
+void TLC_AllClock_Init(void);
+void setTLCChannel(uint8_t channel, uint16_t val);
+void Timer_Init(void);
+void TLC_AllClock_Init(void);
+void TLC_Pin_Init(void);
+void PWM_Timer_Init_TLC(void);
+void resetTLC();
+void writeTLC(uint8_t data);
+void updateTLC();
+
+/*Nove Funkcije*/
+
+void init_tim7(void);
+void TIM7_IRQHandler(void);
+uint8_t SPI1_send(uint8_t data);
+void init_SPI1(void);
+void Tlc5940_setAllDC(uint8_t value);
+void Blank_Pulse(void);
+void TLC_One_Led_On(uint8_t led_num);
+#endif
---
+++ src/tlc5940.c
@@ -0,0 +1,516 @@
+#include "tlc5940.h"
+#include "tm_stm32f4_delay.h"
+
+uint8_t TLC5940_count;
+uint16_t *TLC5940_grayScale;
+
+extern int timer_update;
+
+void TLC_Pin_Init(void);
+
+//replace TIM2 with your desired Timer...
+void TIM3_IRQHandler(void)
+{
+	 TIM_ClearITPendingBit(TIM3, TIM_IT_Update);
+	 int i;
+	 for (i = 0; i < 4096; i++){
+			 		TLC5940_GPIO->BSRRL = PIN_GSCLK;
+			 		TLC5940_GPIO->BSRRH = PIN_GSCLK;
+			 				}
+			 Blank_Pulse();
+}
+
+void setTLCChannel(uint8_t channel, uint16_t val)
+{
+    leds[channel] = (val & 0x0FFF);
+}
+
+//Tle tocn nvm zakaj je potrebno timer TIM3 enablat
+void Timer_Init()
+{
+    RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM3, ENABLE);
+
+
+    TIM_TimeBaseInitTypeDef TIM_TimeBase_InitStructure;
+    NVIC_InitTypeDef NVIC_InitStructure;
+
+    //init 1 ms timer
+    TIM_TimeBase_InitStructure.TIM_ClockDivision = TIM_CKD_DIV1;
+    TIM_TimeBase_InitStructure.TIM_CounterMode = TIM_CounterMode_Up;
+    TIM_TimeBase_InitStructure.TIM_Period = 1;
+    TIM_TimeBase_InitStructure.TIM_Prescaler = 35999;
+    TIM_TimeBaseInit(TIM3, &TIM_TimeBase_InitStructure);
+
+    TIM_ITConfig(TIM3, TIM_IT_Update, ENABLE);
+
+
+    NVIC_InitStructure.NVIC_IRQChannel = TIM3_IRQn;
+    NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;
+    NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 0x0F;
+    NVIC_InitStructure.NVIC_IRQChannelSubPriority = 0x0F;
+    NVIC_Init(&NVIC_InitStructure);
+
+
+
+}
+
+void TLC_AllClock_Init()
+{
+	/* Clock for GPIOD & GPIOB & GPIOC */
+	/**PAZI
+	* Tle je ptrebno clocke zamenjat ce hoces da to dela pravilno zarad SPI-ja PWM-ja.
+	* */
+    RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOD, ENABLE);
+    RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOA, ENABLE);
+    RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOC, ENABLE);
+
+    //Clock on for SPI1
+    RCC_AHB1PeriphClockCmd(RCC_APB2Periph_SPI1, ENABLE);
+
+    //PWM Timer Clock Init
+    RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM2, ENABLE);
+
+}
+
+void TLC_Pin_Init(void)
+{
+	//Before running Init in main must me defined TM_stm32f4_delay!
+	clkCnt = 0;
+	GPIO_InitTypeDef GPIO_InitStruct_GPIOD;
+	GPIO_InitTypeDef GPIO_InitStruct_SPI;
+
+	//SPI structure
+	SPI_InitTypeDef SPI_InitStruct;
+
+
+	//Tle clocke das ce nimas klicane funkcije TLC_AllClock_Init
+
+
+	/** GPIO */
+	/* Set pins XLAT, BLANK, VPRG -> GPIOD */
+	GPIO_InitStruct_GPIOD.GPIO_Pin = DD_XLAT | DD_BLANK ;
+	GPIO_InitStruct_GPIOD.GPIO_OType = GPIO_OType_PP;
+	GPIO_InitStruct_GPIOD.GPIO_Mode = GPIO_Mode_OUT;
+	GPIO_InitStruct_GPIOD.GPIO_Speed = GPIO_Speed_50MHz;
+    GPIO_Init(GPIOD, &GPIO_InitStruct_GPIOD);
+
+    /** SPI PINS
+     * ---------------------------------------------------
+     * 						SPI
+     * ---------------------------------------------------
+     *  SPI1
+     *  To pomen da mamo na: http://stm32f4-discovery.net/2014/04/library-05-spi-for-stm32f4xx/
+     *  GPIOA
+     *  MISO -> GPIO_Pin_6
+     *  MOSI -> GPIO_Pin_7
+     *  SCL  -> GPIO_Pin_5
+     * */
+
+    //Vsi pini init ZA GPIO B
+     GPIO_InitStruct_SPI.GPIO_Pin = GPIO_Pin_All;
+     GPIO_InitStruct_SPI.GPIO_Mode = GPIO_Mode_OUT;
+     GPIO_InitStruct_SPI.GPIO_OType = GPIO_OType_PP;
+     GPIO_InitStruct_SPI.GPIO_Speed = GPIO_Speed_50MHz;
+     GPIO_InitStruct_SPI.GPIO_PuPd = GPIO_PuPd_NOPULL;
+     GPIO_Init(TLC5940_GPIO, &GPIO_InitStruct_SPI);
+
+     //Vsi pini init ZA GPIO D -> LEVEL PINS
+      GPIO_InitStruct_SPI.GPIO_Pin = GPIO_Pin_All;
+      GPIO_InitStruct_SPI.GPIO_Mode = GPIO_Mode_OUT;
+      GPIO_InitStruct_SPI.GPIO_OType = GPIO_OType_PP;
+      GPIO_InitStruct_SPI.GPIO_Speed = GPIO_Speed_50MHz;
+      GPIO_InitStruct_SPI.GPIO_PuPd = GPIO_PuPd_NOPULL;
+      GPIO_Init(PORTx_PIN_LVL, &GPIO_InitStruct_SPI);
+
+	/* Set pins SCLK, MOSI->"SIN" -> GPIOA */
+     /* configure pins used by SPI1
+     * PA5 = SCK
+     * PA6 = MISO
+     * PA7 = MOSI
+     */
+    GPIO_InitStruct_SPI.GPIO_Pin = GPIO_Pin_7 | GPIO_Pin_6 | GPIO_Pin_5;
+    GPIO_InitStruct_SPI.GPIO_OType = GPIO_OType_PP;
+    GPIO_InitStruct_SPI.GPIO_PuPd = GPIO_PuPd_NOPULL;
+    GPIO_InitStruct_SPI.GPIO_Mode = GPIO_Mode_AF;
+    GPIO_InitStruct_SPI.GPIO_Speed = GPIO_Speed_50MHz;
+    GPIO_Init(PORTx_SPI, &GPIO_InitStruct_SPI);				//GPIOA
+
+
+    GPIO_PinAFConfig(GPIOA, GPIO_PinSource5, GPIO_AF_SPI1);
+    GPIO_PinAFConfig(GPIOA, GPIO_PinSource6, GPIO_AF_SPI1);
+    GPIO_PinAFConfig(GPIOA, GPIO_PinSource7, GPIO_AF_SPI1);
+
+    //SPI
+
+    RCC_APB2PeriphClockCmd(RCC_APB2Periph_SPI1, ENABLE);
+
+    SPI_InitStruct.SPI_Direction = SPI_Direction_2Lines_FullDuplex; // set to full duplex mode, seperate MOSI and MISO lines
+    SPI_InitStruct.SPI_Mode = SPI_Mode_Master; // transmit in master mode, NSS pin has to be always high
+    SPI_InitStruct.SPI_DataSize = SPI_DataSize_8b; // one packet of data is 8 bits wide
+    SPI_InitStruct.SPI_CPOL = SPI_CPOL_Low; // clock is low when idle
+    SPI_InitStruct.SPI_CPHA = SPI_CPHA_1Edge; // data sampled at first edge
+    SPI_InitStruct.SPI_NSS = SPI_NSS_Soft | SPI_NSSInternalSoft_Set; // set the NSS management to internal and pull internal NSS high
+    SPI_InitStruct.SPI_BaudRatePrescaler = SPI_BaudRatePrescaler_16; // SPI frequency is APB2 frequency / 4
+    SPI_InitStruct.SPI_FirstBit = SPI_FirstBit_MSB;// data is transmitted MSB first
+    SPI_Init(SPI1, &SPI_InitStruct);
+
+    //enable SPI
+    SPI_Cmd(SPI1, ENABLE);
+
+    //initial setup
+     // GPIO_WriteBit(PORTx_TLC5940, DD_BLANK | DD_XLAT, RESET);
+
+    //disable all outputs
+   // resetTLC();
+
+}
+void PWM_Timer_Init_TLC(void)
+{
+
+	/*****************************************************
+				Init of the PWM Timer
+	******************************************************/
+	// RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM2EN, ENABLE);
+	//Druga ura
+	 RCC_APB1PeriphClockCmd(RCC_APB1ENR_TIM2EN,ENABLE);
+	 RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM2, ENABLE);
+
+	//Define structure names
+	GPIO_InitTypeDef GPIO_InitStruct_GSCLK;
+	TIM_TimeBaseInitTypeDef TIM_TimeBase_InitStructure;
+	TIM_OCInitTypeDef TIM_OC_InitStructure;
+	NVIC_InitTypeDef NVIC_InitStructure;
+
+	GPIO_InitStruct_GSCLK.GPIO_Mode = GPIO_Mode_AF;
+	GPIO_InitStruct_GSCLK.GPIO_Pin  = DD_GSCLK;
+	GPIO_InitStruct_GSCLK.GPIO_Speed= GPIO_Speed_50MHz;
+
+	GPIO_Init(PORTx_GSCLK, &GPIO_InitStruct_GSCLK);
+
+	/* Timer Init TIM2 */
+
+
+	TIM_ITConfig(TIM2, TIM_IT_Update, ENABLE);
+
+
+	// Timer starten
+	TIM_Cmd(TIM2, ENABLE);
+
+	TIM_TimeBase_InitStructure.TIM_Prescaler = 2;
+	TIM_TimeBase_InitStructure.TIM_CounterMode = TIM_CounterMode_Up;
+	TIM_TimeBase_InitStructure.TIM_Period = 4095;
+	TIM_TimeBase_InitStructure.TIM_ClockDivision = TIM_CKD_DIV1;
+	TIM_TimeBaseInit(TIM2, &TIM_TimeBase_InitStructure);	//50% duty cycle
+
+	//replace TIM2 with your desired timer and TIM_OCxInit to match your PWM port. In this case TIM2_CH2 is used (PA0)
+	//TIM_OC1Init(TIM2, &TIM_OC_InitStructure);
+
+	NVIC_InitStructure.NVIC_IRQChannel = TIM2_IRQn;
+	NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;
+	NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 0x01;
+	NVIC_InitStructure.NVIC_IRQChannelSubPriority = 0x00;
+	NVIC_Init(&NVIC_InitStructure);
+
+	NVIC_Init(&NVIC_InitStructure);
+
+	//replace TIM2 with your desired timer...
+	TIM_ITConfig(TIM2, TIM_IT_Update, ENABLE);
+
+	//insert hand timer to init struct
+
+
+	//To bomo dal vn
+	//initTLC(&tlcInit);
+}
+
+
+
+void resetTLC()
+{
+	uint8_t i;
+	for(i = 0; i < 16; i++){
+		leds[i] = 0x0000;
+	}
+	updateTLC();
+}
+
+//write a byte to the chip
+void writeTLC(uint8_t data)
+{
+    SPI_I2S_SendData(SPI1, data);
+    while (SPI_I2S_GetFlagStatus(SPI1, SPI_I2S_FLAG_TXE) == RESET);
+}
+
+void updateTLC()
+{
+	//wait till GSCLK cycle to finish
+	while(clkCnt);
+	//disable GSCLK output
+	TIM_Cmd(TIM2, DISABLE);
+	GPIO_WriteBit(PORTx_TLC5940, DD_BLANK, SET);
+	Delayms(1);
+
+	Delayms(1);
+	    int8_t i;
+	    for (i = 15; i >= 0; i-=2)
+	    {
+	        uint8_t send1 = 0;
+	        uint8_t send = leds[i] >> 4;
+	        writeTLC(send);
+
+	        send = (leds[i] & 0x000F);
+	        send <<= 4;
+	        send1 = (leds[i-1]) >> 8;
+
+	        send |= send1;
+	        writeTLC(send);
+
+	        send = leds[i-1];
+	        writeTLC(send);
+	    }
+	    //delay(1);
+	    GPIO_WriteBit(PORTx_TLC5940, DD_XLAT, SET);
+	    Delayms(1);
+	    GPIO_WriteBit(PORTx_TLC5940, DD_BLANK, RESET);
+	    GPIO_WriteBit(PORTx_TLC5940, DD_XLAT, RESET);
+	    Delayms(1);
+	    clkCnt = 0;
+	    TIM_Cmd(TIM2, ENABLE);
+
+}
+
+void Tlc5940_setAllDC(uint8_t value)
+{
+	int i;
+    uint8_t firstByte = value << 2 | value >> 4;
+    uint8_t secondByte = value << 4 | value >> 2;
+    uint8_t thirdByte = value << 6 | value;
+
+	GPIO_SetPinHigh(TLC5940_GPIO,DD_VPRG);
+	GPIO_SetPinHigh(TLC5940_GPIO,PIN_BLANK);
+
+    for (i = 0; i < NUM_TLCS * 12; i += 3) {
+    	SPI1_send(firstByte);
+    	SPI1_send(secondByte);
+    	SPI1_send(thirdByte);
+    }
+
+    //pulse XLAT
+	GPIO_SetPinHigh(TLC5940_GPIO,DD_XLAT);
+	 _delay(1);
+	GPIO_SetPinLow(TLC5940_GPIO,PIN_BLANK);
+	 _delay(1);
+	GPIO_SetPinLow(TLC5940_GPIO,DD_XLAT);
+
+
+
+}
+
+void TLC_One_Led_On(uint8_t led_num)
+{
+	uint8_t n;
+	u16 data[COUNT_TLC* 16]={0x0000 ,0x0000,0x0000,0x0000,0x0000,0x0000,0x0000,0x0000,0x0000,0x0000,0x0000,0x0000,0x0000,0x0000,0x0000,0x0000};
+
+//	TIM_Cmd(TIM3, DISABLE);
+
+
+	GPIO_SetPinLow(TLC5940_GPIO,PIN_VPRG);
+	data[led_num] = 4095;
+	 //Test
+	 GPIO_SetPinHigh(TLC5940_GPIO,PIN_BLANK);
+	 for(n=0; n<(COUNT_TLC *16); n=n+2){
+		 SPI1_send(data[n]>>4);
+		 SPI1_send((data[n]<<4) | (data[n+1])>>8);
+		 SPI1_send(data[n+1]);
+	 }
+
+	 _delay(1);
+	 GPIO_SetPinHigh(TLC5940_GPIO,PIN_XLAT);
+	 _delay(1);
+	 GPIO_SetPinLow(TLC5940_GPIO,PIN_BLANK);
+	 GPIO_SetPinLow(TLC5940_GPIO,PIN_XLAT);
+
+
+	//TIM_Cmd(TIM3, ENABLE);
+}
+
+void TLC_here()
+{
+
+	GPIO_SetPinLow(PORTx_PIN_LVL, PIN_LEVEL_4);
+	TLC_One_Led_On(0);
+
+	GPIO_SetPinHigh(PORTx_PIN_LVL, PIN_LEVEL_4);
+
+
+
+}
+
+
+void init_tim7(void){
+TIM_TimeBaseInitTypeDef TIM_TBInitStruct;
+NVIC_InitTypeDef NVIC_InitStructure;
+
+RCC_APB1PeriphClockCmd(RCC_APB1ENR_TIM7EN,ENABLE); // Timer saat kaynağını aç
+TIM_TBInitStruct.TIM_Prescaler =4199; // ÖnBölücü (PSC) değeri 4199
+TIM_TBInitStruct.TIM_Period =9; // Otomatik geri yükleme (ARR) eşiği 1
+TIM_TimeBaseInit(TIM7,&TIM_TBInitStruct); // Timeri init et
+
+TIM_ITConfig(TIM7, TIM_IT_Update, ENABLE); // Timer Update olayında interrupt istesin.
+NVIC_InitStructure.NVIC_IRQChannel = TIM7_IRQn;
+NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;
+NVIC_Init(&NVIC_InitStructure);
+
+
+TIM_Cmd(TIM7, ENABLE);// Timeri başlat
+}
+
+void TIM7_IRQHandler(void){
+
+TIM7->SR=0;
+GPIOD->ODR ^= GPIO_Pin_12;
+
+timer_update +=1;
+}
+
+void init_SPI1(void){
+
+GPIO_InitTypeDef GPIO_InitStruct;
+SPI_InitTypeDef SPI_InitStruct;
+
+// enable clock for used IO pins
+RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOA | RCC_AHB1Periph_GPIOD| TLC_RCC_AHB1, ENABLE);
+
+
+GPIO_InitStruct.GPIO_Pin = GPIO_Pin_All;
+GPIO_InitStruct.GPIO_Mode = GPIO_Mode_OUT;
+GPIO_InitStruct.GPIO_OType = GPIO_OType_PP;
+GPIO_InitStruct.GPIO_Speed = GPIO_Speed_50MHz;
+GPIO_InitStruct.GPIO_PuPd = GPIO_PuPd_NOPULL;
+GPIO_Init(GPIOD, &GPIO_InitStruct);
+
+GPIO_InitStruct.GPIO_Pin = GPIO_Pin_All;
+GPIO_InitStruct.GPIO_Mode = GPIO_Mode_OUT;
+GPIO_InitStruct.GPIO_OType = GPIO_OType_PP;
+GPIO_InitStruct.GPIO_Speed = GPIO_Speed_50MHz;
+GPIO_InitStruct.GPIO_PuPd = GPIO_PuPd_NOPULL;
+GPIO_Init(TLC5940_GPIO, &GPIO_InitStruct);
+
+/* configure pins used by SPI1
+* PA5 = SCK
+* PA6 = MISO
+* PA7 = MOSI
+*/
+GPIO_InitStruct.GPIO_Pin = GPIO_Pin_7 | GPIO_Pin_5;
+GPIO_InitStruct.GPIO_Mode = GPIO_Mode_AF;
+GPIO_InitStruct.GPIO_OType = GPIO_OType_PP;
+GPIO_InitStruct.GPIO_Speed = GPIO_Speed_50MHz;
+GPIO_InitStruct.GPIO_PuPd = GPIO_PuPd_NOPULL;
+GPIO_Init(GPIOA, &GPIO_InitStruct);
+
+// connect SPI1 pins to SPI alternate function
+GPIO_PinAFConfig(GPIOA, GPIO_PinSource5, GPIO_AF_SPI1);
+
+GPIO_PinAFConfig(GPIOA, GPIO_PinSource7, GPIO_AF_SPI1);
+
+// enable clock for used IO pins
+RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOE, ENABLE);
+
+/* Configure the chip select pin
+in this case we will use PE7 */
+GPIO_InitStruct.GPIO_Pin = GPIO_Pin_7;
+GPIO_InitStruct.GPIO_Mode = GPIO_Mode_OUT;
+GPIO_InitStruct.GPIO_OType = GPIO_OType_PP;
+GPIO_InitStruct.GPIO_Speed = GPIO_Speed_50MHz;
+GPIO_InitStruct.GPIO_PuPd = GPIO_PuPd_UP;
+GPIO_Init(GPIOE, &GPIO_InitStruct);
+
+GPIOE->BSRRL |= GPIO_Pin_7; // set PE7 high
+
+// enable peripheral clock
+RCC_APB2PeriphClockCmd(RCC_APB2Periph_SPI1, ENABLE);
+
+/* configure SPI1 in Mode 0
+* CPOL = 0 --> clock is low when idle
+* CPHA = 0 --> data is sampled at the first edge
+*/
+SPI_InitStruct.SPI_Direction = SPI_Direction_2Lines_FullDuplex; // set to full duplex mode, seperate MOSI and MISO lines
+SPI_InitStruct.SPI_Mode = SPI_Mode_Master; // transmit in master mode, NSS pin has to be always high
+SPI_InitStruct.SPI_DataSize = SPI_DataSize_8b; // one packet of data is 8 bits wide
+SPI_InitStruct.SPI_CPOL = SPI_CPOL_Low; // clock is low when idle
+SPI_InitStruct.SPI_CPHA = SPI_CPHA_1Edge; // data sampled at first edge
+SPI_InitStruct.SPI_NSS = SPI_NSS_Soft | SPI_NSSInternalSoft_Set; // set the NSS management to internal and pull internal NSS high
+SPI_InitStruct.SPI_BaudRatePrescaler = SPI_BaudRatePrescaler_64; // SPI frequency is APB2 frequency / 4
+SPI_InitStruct.SPI_FirstBit = SPI_FirstBit_MSB;// data is transmitted MSB first
+SPI_Init(SPI1, &SPI_InitStruct);
+
+SPI_Cmd(SPI1, ENABLE); // enable SPI1
+}
+
+/* This funtion is used to transmit and receive data
+* with SPI1
+* data --> data to be transmitted
+* returns received value
+*/
+uint8_t SPI1_send(uint8_t data){
+
+SPI_I2S_SendData(SPI1,data);
+//SPI1->DR = data; // write data to be transmitted to the SPI data register
+while( !(SPI1->SR & SPI_I2S_FLAG_TXE) ); // wait until transmit complete
+while( !(SPI1->SR & SPI_I2S_FLAG_RXNE) ); // wait until receive complete
+while( SPI1->SR & SPI_I2S_FLAG_BSY ); // wait until SPI is not busy anymore
+return SPI1->DR; // return received data from SPI data register
+}
+
+void _delay(int value){
+while(value--)
+{
+
+}
+}
+
+void Blank_Pulse(){
+
+	GPIO_SetPinHigh(TLC5940_GPIO, PIN_BLANK);
+	GPIO_SetPinLow(TLC5940_GPIO,PIN_BLANK);
+//TLC5940_GPIO->ODR |=BLANK_PIN;
+//TLC5940_GPIO->ODR &=0x00000000;
+}
+
+void Xlat_Pulse(){
+
+	GPIO_SetPinHigh(TLC5940_GPIO, PIN_XLAT);
+	GPIO_SetPinLow(TLC5940_GPIO,PIN_XLAT);
+
+//TLC5940_GPIO->ODR |=XLAT_PIN;
+//TLC5940_GPIO->ODR &=0x00000000;
+
+}
+
+void GSCLK_Pulse(){
+	GPIO_SetPinHigh(TLC5940_GPIO,PIN_GSCLK);
+	GPIO_SetPinLow(TLC5940_GPIO,PIN_GSCLK);
+//TLC5940_GPIO->ODR |=GSCLK_PIN;
+//TLC5940_GPIO->ODR &=0x00000000;
+
+}
+
+//CHECK CORE CLK
+void MCO2_INIT(){
+GPIO_InitTypeDef GPIO_InitStructure;
+
+// Output HSE clock (8 MHz) on MCO pin (PC9)
+RCC_MCO2Config(RCC_MCO2Source_SYSCLK, RCC_MCO2Div_5);
+
+RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOC, ENABLE);
+
+GPIO_StructInit(&GPIO_InitStructure);
+
+GPIO_InitStructure.GPIO_Pin = GPIO_Pin_9;
+GPIO_InitStructure.GPIO_Speed = GPIO_Speed_100MHz;
+GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF;
+GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
+GPIO_Init(GPIOC, &GPIO_InitStructure);
+GPIO_PinAFConfig(GPIOC, GPIO_PinSource9, GPIO_AF_MCO);
+
+}

2145bde071a7e3e187bbdb2756db94db3ff5dcbd
Tine Bertoncelj <tine.bertoncelj@gmail.com>
2018-06-06 23:55:16 +0200

    Added comments

--- inc/tlc5940.h
+++ inc/tlc5940.h
@@ -7,60 +7,21 @@
 #include "stm32f4xx_gpio.h"
 #include "defines.h"
 
-#define NUM_TLCS 3
-#define COUNT_TLC 3
-
-/*SPI1
-  *  GPIOA
-  *  MOSI -> GPIO_Pin_7
-  *  SCL  -> GPIO_Pin_5
- */
-
- #define TLC_RCC_AHB1 RCC_AHB1Periph_GPIOB
- #define TLC5940_GPIO 	GPIOB
- #define PIN_BLANK		GPIO_Pin_5
- #define PIN_VPRG 		GPIO_Pin_9
- #define PIN_XLAT 		GPIO_Pin_6
- #define PIN_GSCLK		GPIO_Pin_3
-
-
-#define TLC5940_GPIO 	GPIOB
-
-#define PORTx_SPI 	 GPIOA
-#define DD_MOSI 	 GPIO_Pin_7		//SIN -> MOSI
-#define DD_SCK 		 GPIO_Pin_5		//Clock SCLK
-
-
-#define PORTx_TLC5940 	GPIOB
-#define DD_XLAT		 	GPIO_Pin_6
-#define DD_BLANK	 	GPIO_Pin_5
-#define DD_VPRG			GPIO_Pin_9		//MISO -> VPRG
-
-
-#define PORTx_GSCLK 	GPIOB
-#define DD_GSCLK 		GPIO_Pin_3	//ta je PWM ju definiran
-
-#define PORTx_PIN_LVL 	GPIOD
-#define PIN_LEVEL_1  	GPIO_Pin_7
-#define PIN_LEVEL_2	    GPIO_Pin_0
-#define PIN_LEVEL_3 	GPIO_Pin_3
-#define PIN_LEVEL_4 	GPIO_Pin_1
-
 //
 /**
- * Pin configuration
+ * Pin configuration TLC5940
  *
  * 		OUT2		OUT1				tlc5940	tlc5940stm
- * 		OUT3	D-> VPRG		PB4		VPRG	MISO
- * 		OUT4		SIN			PD7		SS		MOSI
- * 		OUT5		SCLK		PD5		SCK 	CLK
- * 		OUT6	D->	XLAT		PD3		XLAT	XLAT
- * 		OUT7	D->	BLANK		PD1		BLANK	BLANK
- * 		OUT8	D->	GND
- * 		OUT9	D->	VCC
- * 		OUT10	D->	IREF
- * 		OUT11	D->	DCPRG
- * 		OUT12	D->	GSCLK		PA0		GSCL	PWM GPIO_Pin_0
+ * 		OUT3	D-> VPRG		PIN_VPRG	 GPIO_Pin_9
+ * 		OUT4	D->	SIN			PIN_MOSI 	 GPIO_Pin_7
+ * 		OUT5	D->	SCLK		PIN_SCK  	 GPIO_Pin_5
+ * 		OUT6	D->	XLAT		PIN_XLAT     GPIO_Pin_6
+ * 		OUT7	D->	BLANK		PIN_BLANK	 GPIO_Pin_5
+ * 		OUT8		GND			GND
+ * 		OUT9		VCC			3V
+ * 		OUT10		IREF		4kOhm TO GND
+ * 		OUT11		DCPRG		3V
+ * 		OUT12	D->	GSCLK		PIN_GSCLK	 GPIO_Pin_3
  * 		OUT13		SOUT
  * 		OUT14		XERR
  * 		OUT15		OUT16
@@ -88,10 +49,10 @@
 //array holding the data to be shifted in the driver
 
 //MAIN 4 ARRAYS FOR LAYERS
-uint16_t data_lvl1[COUNT_TLC * 16];
-uint16_t data_lvl2[COUNT_TLC * 16];
-uint16_t data_lvl3[COUNT_TLC * 16];
-uint16_t data_lvl4[COUNT_TLC * 16];
+uint16_t data_lvl1[NUM_TLCS * 16];
+uint16_t data_lvl2[NUM_TLCS * 16];
+uint16_t data_lvl3[NUM_TLCS * 16];
+uint16_t data_lvl4[NUM_TLCS * 16];
 
 volatile uint16_t clkCnt;
 
@@ -101,7 +62,7 @@ uint8_t SPI1_send(uint8_t data);
 void init_TLC_Pins_SPI(void);
 void Tlc5940_setAllDC(uint8_t value);
 void TLC_Update_lvl(uint16_t *data_lvl);
-void TLC_here(void);
+void TLC_Cube_Update(void);
 void GSCLK_Pulzes(void);
 
 void _delay(int value);
--- src/tlc5940.c
+++ src/tlc5940.c
@@ -1,6 +1,17 @@
 #include "tlc5940.h"
 #include "tm_stm32f4_delay.h"
+#include "defines.h"
 
+/** \mainpage
+    The <"http://www.ti.com/lit/gpn/TLC5940">Texas Instruments TLC5940
+    is a 16-channel, constant-current sink LED driver.  Each channel has
+    an individually adjustable 4096-step grayscale PWM brightness control and
+    a 64-step, constant-current sink (no LED resistors needed!).  This chip
+    is a current sink, so be sure to use common anode RGB LEDs.
+
+    All the options for the library are located in defines.h, including
+    #NUM_TLCS, what pins to use, and the PWM period.
+*/
 uint8_t TLC5940_count;
 uint16_t *TLC5940_grayScale;
 
@@ -10,26 +21,37 @@ extern int timer_update;
 void TIM3_IRQHandler(void)
 {
     TIM_ClearITPendingBit(TIM3, TIM_IT_Update);
-    TLC_here();
+    TLC_Cube_Update();
 }
 
-
-//STAY
+/**
+ * Cycle through 0-4045 pulse width modulation (PWM) values for grayscale brightness control.
+ * First all data loaded into Input Shift Register is pushed onward by VPRG pulse.
+ * Then GCLK is pulsing 4096 times to get over all his data values. This signal can be as high as 30 MHz.
+ * Setting BLANK to high and then low causes the counter to be set to zero.
+ * An incoming clock pulse causes the counter to be incremented by 1.
+ * The chip then turns on all of the outputs which have a greyscale value which is non-zero.
+ * Each subsequent pulse cause the chip to do the following:
+ *	1.) Increment the counter
+	2.)For each output compare the counter with the greyscale value for the output pin.
+	If the value is greater than the counter then turn the output on, else turn the output off.
+ * */
 void GSCLK_Pulzes()
 {
-	//TIM_ClearITPendingBit(TIM3, TIM_IT_Update);
 	int i;
-	TLC5940_GPIO->BSRRL = PIN_VPRG;
-	TLC5940_GPIO->BSRRH = PIN_VPRG;
+	PORTx_TLC5940->BSRRL = PIN_VPRG;
+	PORTx_TLC5940->BSRRH = PIN_VPRG;
 	for (i = 0; i < 4096; i++){
-		TLC5940_GPIO->BSRRL = PIN_GSCLK;
-		TLC5940_GPIO->BSRRH = PIN_GSCLK;
+		PORTx_TLC5940->BSRRL = PIN_GSCLK;
+		PORTx_TLC5940->BSRRH = PIN_GSCLK;
 	}
-		 Blank_Pulse();
+	Blank_Pulse();
 }
-
-
-//STAY
+/**
+ * Change TIM3 to your preferred timer.
+ * TIMER_PERIOD and TIMER_PERIOD are set in defines.h
+ * They are set for 20ms timer interrupt.
+ * */
 void Timer_Init()
 {
     RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM3, ENABLE);
@@ -39,8 +61,8 @@ void Timer_Init()
 
     TIM_TimeBase_InitStructure.TIM_ClockDivision = TIM_CKD_DIV1;
     TIM_TimeBase_InitStructure.TIM_CounterMode = TIM_CounterMode_Up;
-    TIM_TimeBase_InitStructure.TIM_Period = 27;  	   			//  27    for 20ms interrupt
-    TIM_TimeBase_InitStructure.TIM_Prescaler = 59999 ;			 // 59999
+    TIM_TimeBase_InitStructure.TIM_Period = TIMER_PERIOD;  	   		//     for 20ms interrupt
+    TIM_TimeBase_InitStructure.TIM_Prescaler = TIMER_PRESCALER ;   //
     TIM_TimeBaseInit(TIM3, &TIM_TimeBase_InitStructure);
 
     TIM_ITConfig(TIM3, TIM_IT_Update, ENABLE);
@@ -52,8 +74,18 @@ void Timer_Init()
     NVIC_Init(&NVIC_InitStructure);
 }
 
-
-//STAY
+/**Setting DOT Correction
+ * The TLC5940 has the capability to fine adjust the output current of each channel OUT0 to OUT15 independently.
+ *	This is also called dot correction. This feature is used to adjust the brightness deviations of LEDs connected to
+ *	the output channels OUT0 to OUT15. Each of the 16 channels can be programmed with a 6-bit word. The
+ *	channel output can be adjusted in 64 steps from 0% to 100% of the maximum output current Imax. Dot correction
+ *	for all channels must be entered at the same time. Equation 7 determines the output current for each output n.
+ *
+ *	VPRG must be set HIGH, and BLANK HIGH to shut down output. As data is send in 8-bits it must be stitch together
+ *	by 6 bit from first data plus 2 bit from second, next is other left 4-bits from second data and 2-bits from third.
+ *
+ *	A low-high XLAT pulse on this pin latches the data received from the microcontroller into the DC register.
+ * */
 void Tlc5940_setAllDC(uint8_t value)
 {
 	int i;
@@ -61,8 +93,8 @@ void Tlc5940_setAllDC(uint8_t value)
     uint8_t secondByte = value << 4 | value >> 2;
     uint8_t thirdByte = value << 6 | value;
 
-	GPIO_SetPinHigh(TLC5940_GPIO,DD_VPRG);
-	GPIO_SetPinHigh(TLC5940_GPIO,PIN_BLANK);
+	GPIO_SetPinHigh(PORTx_TLC5940,PIN_VPRG);
+	GPIO_SetPinHigh(PORTx_TLC5940,PIN_BLANK);
 
     for (i = 0; i < NUM_TLCS * 12; i += 3) {
     	SPI1_send(firstByte);
@@ -71,42 +103,57 @@ void Tlc5940_setAllDC(uint8_t value)
     }
 
     //pulse XLAT
-	GPIO_SetPinHigh(TLC5940_GPIO,DD_XLAT);
+	GPIO_SetPinHigh(PORTx_TLC5940,PIN_XLAT);
 	 _delay(1);
-	GPIO_SetPinLow(TLC5940_GPIO,PIN_BLANK);
+	GPIO_SetPinLow(PORTx_TLC5940,PIN_BLANK);
 	 _delay(1);
-	GPIO_SetPinLow(TLC5940_GPIO,DD_XLAT);
+	GPIO_SetPinLow(PORTx_TLC5940,PIN_XLAT);
 }
 
-
-//STAY
+/**
+ * The TLC5940 uses 12 bits to determine the output level for the LEDs connected.
+ * Each LED is controlled independently and the full array of 12 bits requires a total of 192 bits (12 bits x 16 LEDs)
+ *  of data to determine the output levels for the LEDs. The 12 bits for each LED gives a total of 4096
+ *  levels for each LED (0 to 4095 inclusive). The TLC5940 uses this data and the current counter to determine the output state of each OUT pin.
+ *
+ * Here we are sending data trough SPI 8-bit data channel.
+ * 1.We take our first 8-bits data and send it.
+ * 2.From first data we take last 4-bits and stitch together 4-bits from second data.
+ * 3.We then send rest of second 8-bits.
+ *
+ * A low-high XLAT pulse on this pin latches the data received from the microcontroller into the GS register.
+ * When BLANK pin is high the outputs are turned off. Setting the pin low turns the outputs back on.
+ * */
 void TLC_Update_lvl(uint16_t *data_lvl)
 {
 	uint8_t n;
 	uint8_t tim = 3;
 
-	 GPIO_SetPinHigh(TLC5940_GPIO,PIN_BLANK);
-	 _delay(tim);
-	 for(n=0; n<(COUNT_TLC *16); n=n+2){
-		 SPI1_send(data_lvl[n]>>4);
-		 SPI1_send((data_lvl[n]<<4) | (data_lvl[n+1])>>8);
-		 SPI1_send(data_lvl[n+1]);
-	 }
+	GPIO_SetPinHigh(PORTx_TLC5940,PIN_BLANK);
+	_delay(tim);
+	for(n=0; n<(NUM_TLCS *16); n=n+2){
+		SPI1_send(data_lvl[n]>>4);
+		SPI1_send((data_lvl[n]<<4) | (data_lvl[n+1])>>8);
+		SPI1_send(data_lvl[n+1]);
+	}
 
-	 _delay(tim);
-	 GPIO_SetPinHigh(TLC5940_GPIO,PIN_XLAT);
-	 _delay(tim);
-	 GPIO_SetPinLow(TLC5940_GPIO,PIN_BLANK);
-	 _delay(tim);
-	 GPIO_SetPinLow(TLC5940_GPIO,PIN_XLAT);
-	 _delay(tim);
+	_delay(tim);
+	GPIO_SetPinHigh(PORTx_TLC5940,PIN_XLAT);
+	_delay(tim);
+	GPIO_SetPinLow(PORTx_TLC5940,PIN_BLANK);
+	_delay(tim);
+	GPIO_SetPinLow(PORTx_TLC5940,PIN_XLAT);
+	_delay(tim);
 
-	  GSCLK_Pulzes();
-	// TIM_Cmd(TIM3, ENABLE);
+	 GSCLK_Pulzes();
 }
 
-//STAY
-void TLC_here()
+/**
+ * Updating all 4 cube layers separately.
+ * Starting to turn on MOSFET voltage on LED, so that TLC can control on/off state
+ * by pulling to the ground. Then we update on layer, flash all LEDs and turn that layer MOSFET off.
+ * */
+void TLC_Cube_Update()
 {
 	GPIO_SetPinHigh(PORTx_PIN_LVL, PIN_LEVEL_4);
 	TLC_Update_lvl(&data_lvl1[0]);
@@ -128,14 +175,13 @@ void TLC_here()
 	GPIO_SetPinLow(PORTx_PIN_LVL, PIN_LEVEL_1);
 }
 
-//STAY
 void init_TLC_Pins_SPI(void){
 
 	GPIO_InitTypeDef GPIO_InitStruct;
 	SPI_InitTypeDef SPI_InitStruct;
 
 	// enable clock for used IO pins
-	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOA | RCC_AHB1Periph_GPIOD| TLC_RCC_AHB1, ENABLE);
+	RCC_AHB1PeriphClockCmd(TLC_RCC_AHA1 | TLC_RCC_AHD1| TLC_RCC_AHB1, ENABLE);
 
 	//GPIO D -> LEVEL PINS
 	GPIO_InitStruct.GPIO_Pin = GPIO_Pin_All;
@@ -151,7 +197,7 @@ void init_TLC_Pins_SPI(void){
 	GPIO_InitStruct.GPIO_OType = GPIO_OType_PP;
 	GPIO_InitStruct.GPIO_Speed = GPIO_Speed_50MHz;
 	GPIO_InitStruct.GPIO_PuPd = GPIO_PuPd_NOPULL;
-	GPIO_Init(TLC5940_GPIO, &GPIO_InitStruct);
+	GPIO_Init(PORTx_TLC5940, &GPIO_InitStruct);
 
 	/** SPI PINS
 	* ---------------------------------------------------
@@ -164,7 +210,7 @@ void init_TLC_Pins_SPI(void){
 	*  SCL  -> GPIO_Pin_5
 	*/
 	//GPIO A -> SCLK, MOSI
-	GPIO_InitStruct.GPIO_Pin = DD_SCK | DD_MOSI;
+	GPIO_InitStruct.GPIO_Pin = PIN_SCK | PIN_MOSI;
 	GPIO_InitStruct.GPIO_Mode = GPIO_Mode_AF;
 	GPIO_InitStruct.GPIO_OType = GPIO_OType_PP;
 	GPIO_InitStruct.GPIO_Speed = GPIO_Speed_50MHz;
@@ -174,9 +220,6 @@ void init_TLC_Pins_SPI(void){
 	// connect SPI1 pins to SPI alternate function
 	GPIO_PinAFConfig(PORTx_SPI, GPIO_PinSource5, GPIO_AF_SPI1); //SCK
 	GPIO_PinAFConfig(PORTx_SPI, GPIO_PinSource7, GPIO_AF_SPI1);	//MOSI
-
-	// enable clock for used IO pins
-	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOE, ENABLE);
 
 	// enable peripheral clock
 	RCC_APB2PeriphClockCmd(RCC_APB2Periph_SPI1, ENABLE);
@@ -214,30 +257,28 @@ uint8_t SPI1_send(uint8_t data){
 	return SPI1->DR; // return received data from SPI data register
 }
 
-//STAY
 void _delay(int value)
 {
 	while(value--);
 }
 
-//STAY
 void Blank_Pulse()
 {
-	GPIO_SetPinHigh(TLC5940_GPIO, PIN_BLANK);
-	GPIO_SetPinLow(TLC5940_GPIO,PIN_BLANK);
-	GPIO_SetPinHigh(TLC5940_GPIO,PIN_BLANK);;
+	GPIO_SetPinHigh(PORTx_TLC5940, PIN_BLANK);
+	GPIO_SetPinLow(PORTx_TLC5940,PIN_BLANK);
+	GPIO_SetPinHigh(PORTx_TLC5940,PIN_BLANK);;
 }
 
 void Xlat_Pulse()
 {
-	GPIO_SetPinHigh(TLC5940_GPIO, PIN_XLAT);
-	GPIO_SetPinLow(TLC5940_GPIO,PIN_XLAT);
+	GPIO_SetPinHigh(PORTx_TLC5940, PIN_XLAT);
+	GPIO_SetPinLow(PORTx_TLC5940,PIN_XLAT);
 
 }
 
 void GSCLK_Pulse()
 {
-	GPIO_SetPinHigh(TLC5940_GPIO,PIN_GSCLK);
-	GPIO_SetPinLow(TLC5940_GPIO,PIN_GSCLK);
+	GPIO_SetPinHigh(PORTx_TLC5940,PIN_GSCLK);
+	GPIO_SetPinLow(PORTx_TLC5940,PIN_GSCLK);
 }
 

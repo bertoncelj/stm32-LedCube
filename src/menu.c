bf8505959263f2a22f930257127e84869e46dcad
Tine Bertoncelj <tine.bertoncelj@gmail.com>
2018-06-11 17:33:07 +0200

    select Lvl depth computor

--- src/menu.c
+++ src/menu.c
@@ -17,27 +17,60 @@ int depth_lvl_2;
 
 void chooseGameType(int slider_read_value);
 
+int ChooseDepthComp(char *message, int slider_read_value)
+{
+	led_button_choosen = 0;
+	int	slider_read_new;
+	char *text_display[4] = {"1", "2","3"};
+	TM_HD44780_Puts(5,0,"Comp Lvl:");
+	TM_HD44780_Puts(5,1,message);
+	while(led_button_choosen == 0){
+		TM_ADC_Read(ADC1, ADC_Channel_4);
+		Delayms(1);
+		slider_read_new = TM_ADC_Read(ADC1, ADC_Channel_0);
+		Delayms(1);
+
+		slider_read_new = Read_ADC_differenceMenu(&slider_read_value, &slider_read_new);
+		slider_read_new = slider_read_new/ 1400;
+
+	if(slider_read_value != slider_read_new){
+				slider_read_value = slider_read_new;
+				TM_HD44780_Puts(5,1,message);
+				TM_HD44780_Puts(8,1,text_display[slider_read_value]);
+		}
+		Delayms(20);
+		TM_BUTTON_Update();
+	}
+	//0 depth is useless, so here layers are from 1 to 3
+	return slider_read_value;
+
+}
+
+int Read_ADC_differenceMenu(int *old, int *new)
+{
+	if(*new - *old > 220 ||  *old - *new > 220){
+		return *new;
+	}
+	else return *old;
+}
+
 void menu()
 {
-	static int slider_read_value;
+	int slider_read_value=-1;
 	int	slider_read_new;
 
-	char *text_display[2] = {"Animations", "Game"};
-
-	//goes in ones just ot fill old values with 0
-	if(flag_read_ADC == 0){
-		slider_read_value = 0;
-		flag_read_ADC = 1;
-	}
+	char *text_display[2] = {"Game", "Animations"};
 	led_button_choosen = 0;
+	TM_HD44780_Clear();
 	TM_HD44780_Puts(0,0,"Menu");
-	TM_HD44780_Puts(0,1,"Animations");
 
 	while(led_button_choosen == 0){
 		TM_ADC_Read(ADC1, ADC_Channel_4);
+		Delayms(1);
 		slider_read_new = TM_ADC_Read(ADC1, ADC_Channel_0);
+		Delayms(1);
 
-		slider_read_new = Read_ADC_difference(&slider_read_value, &slider_read_new);
+		slider_read_new = Read_ADC_differenceMenu(&slider_read_value, &slider_read_new);
 		slider_read_new = slider_read_new/ 2300;
 
 		if(slider_read_value != slider_read_new){
@@ -47,10 +80,11 @@ void menu()
 			slider_read_value = slider_read_new;
 			if(slider_read_value == 1)TM_HD44780_Puts(0, 1, text_display[1]);
 			else TM_HD44780_Puts(0,1, text_display[0]);
+
 		}
 		TM_BUTTON_Update();
 	}
-	if(slider_read_value == 1) chooseGameType(slider_read_value);
+	if(slider_read_value == 0) chooseGameType(slider_read_value);
 	else ANIM_LIST_constat();
 }
 
@@ -60,20 +94,23 @@ void chooseGameType(int slider_read_value)
 	int	slider_read_new;
 	char *text_display[4] = {"PvsP", "PvsC ","CvsC","Empty"};
 
+	TM_HD44780_Puts(0,1," vs ");
 	while(led_button_choosen == 0){
-		TM_ADC_Read(ADC1, ADC_Channel_4);	//dummy read
+		TM_ADC_Read(ADC1, ADC_Channel_4);
+		Delayms(1);
 		slider_read_new = TM_ADC_Read(ADC1, ADC_Channel_0);
+		Delayms(1);
 
-		slider_read_new = Read_ADC_difference(&slider_read_value, &slider_read_new);
-		slider_read_new = slider_read_new/ 1050;
+		slider_read_new = Read_ADC_differenceMenu(&slider_read_value, &slider_read_new);
+		slider_read_new = slider_read_new/ 1400;
 
 		if(slider_read_value != slider_read_new){
-				TM_HD44780_Clear();
 				TM_HD44780_Puts(0,0,"Menu");
 
 				slider_read_value = slider_read_new;
 				TM_HD44780_Puts(0,1,text_display[slider_read_value]);
 		}
+		Delayms(20);
 		TM_BUTTON_Update();
 	}
 	switch(slider_read_value){
@@ -83,12 +120,12 @@ void chooseGameType(int slider_read_value)
 		break;
 
 		case 1:
-			RunGame(GetHumanMove, GetComputerMove,1,1);
+			RunGame(GetHumanMove, GetComputerMove,1,ChooseDepthComp("C1:", slider_read_value));
 
 		break;
 
 		case 2:
-			RunGame(GetComputerMove, GetComputerMove,1,1);
+			RunGame(GetComputerMove, GetComputerMove,ChooseDepthComp("C1:", slider_read_value),ChooseDepthComp("C2:", slider_read_value));
 		break;
 	}
 }
